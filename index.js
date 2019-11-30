d30bc1f25c04a376f7e0b4116d6fae2cdfc37447
Hosein Khansari <hrk.x11@gmail.com>
2019-11-30 11:01:51 +0330

    Add proper error logging

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ if (!process.env.TOKEN) {
   dotenv.config();
 }
 
-const { TOKEN, API_ROOT, BOT_USERNAME } = process.env;
+const {
+  TOKEN, API_ROOT, BOT_USERNAME, LOG_ID
+} = process.env;
 
 const bot = new Telegraf(TOKEN, {
   username: BOT_USERNAME,
@@ -18,30 +20,53 @@ const bot = new Telegraf(TOKEN, {
   }
 });
 
-bot.startPolling();
+bot.context.log = err => {
+  if (LOG_ID) {
+    bot.telegram.sendMessage(LOG_ID, `⛔️ *Error: ${err.message}*
+
+${err}`, {
+      parse_mode: 'Markdown'
+    });
+  } else {
+    console.log(err);
+  }
+};
+bot.catch(err => bot.context.log(err));
 
 bot.start(ctx => ctx.reply(`
 سلام! برای مطلع شدن از وضعیت آلودگی هوای تهران از فرمان /now استفاده کنید.
 `));
 
 bot.command('/now', async ctx => {
-  const { now, last24hours } = await scrape();
-  ctx.reply(writeMessage({ now, last24hours }), {
-    parse_mode: 'Markdown'
-  });
+  try {
+    const data = await scrape();
+    ctx.reply(writeMessage(data), {
+      parse_mode: 'Markdown'
+    });
+  } catch (e) {
+    ctx.reply('خطایی رخ داد. لطفا بعدا امتحان کنید.');
+    ctx.log(e);
+  }
 });
 
 bot.on('inline_query', async ctx => {
-  const { now, last24hours } = await scrape();
-  const message = writeMessage({ now, last24hours });
-  return ctx.answerInlineQuery([{
-    type: 'article',
-    id: ctx.inlineQuery.id,
-    title: 'شاخص آلودگی تهران',
-    description: 'هم‌اکنون',
-    input_message_content: {
-      message_text: message,
-      parse_mode: 'Markdown'
-    }
-  }]);
+  try {
+    const data = await scrape();
+    return ctx.answerInlineQuery([{
+      type: 'article',
+      id: ctx.inlineQuery.id,
+      title: 'شاخص آلودگی تهران',
+      description: 'هم‌اکنون',
+      input_message_content: {
+        message_text: writeMessage(data),
+        parse_mode: 'Markdown'
+      }
+    }]);
+  } catch (e) {
+    bot.log(e);
+    return ctx.answerInlineQuery([]);
+  }
 });
+
+bot.startPolling();
+console.log(`@${BOT_USERNAME} is online...!`);
