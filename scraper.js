d920ef901399bd3154d0591ee28e28f61176c0bf
Hosein Khansari <hrk.x11@gmail.com>
2019-11-30 17:42:02 +0330

    [fix] Throw if html is not as wanted, Set a timeout for requests

--- scraper.js
+++ scraper.js
@@ -3,10 +3,13 @@ import superagent from 'superagent';
 
 /** @param {string} url */
 export async function fetchData(url) {
-  const { text: html } = await superagent.get(url),
-    $ = cheerio.load(html);
-  const numbers = $('.aqival'),
-    index = parseInt(numbers.eq(1).text()),
+  const { text: html } = await superagent.get(url).timeout(30000),
+    $ = cheerio.load(html),
+    numbers = $('.aqival');
+
+  if (!numbers.length) throw new Error('Malformed HTML received');
+
+  const index = parseInt(numbers.eq(1).text()),
     last24hours = parseInt(numbers.eq(0).text());
   return { now: index, last24hours };
 }
